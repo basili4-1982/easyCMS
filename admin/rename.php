4807578e833ed4538efd18bb7a86c247700801ac
v.ahmedjanov <v.ahmedjanov@4slovo.ru>
2016-04-21 11:37:38 +0300

    Админка переименовал html в php

---
+++ admin/rename.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.ahmedjanov
+ * Date: 21.04.2016
+ * Time: 11:19
+ */
+
+$res=array();
+
+function treeDir($path)
+{
+
+    global $res;
+
+
+   $files=glob($path."/*");
+
+    foreach ($files as $file){
+
+        if (is_dir($file)){
+            treeDir($file);
+        }
+        else{
+            if (pathinfo($file,PATHINFO_EXTENSION)=='html')
+            {
+                $res[]=$file;
+            }
+
+        }
+
+    }
+
+    return $res;
+}
+
+
+
+$dirs=treeDir(__DIR__);
+
+
+foreach ($dirs as $file){
+    
+    rename($file,str_replace('.html','.php',$file));
+}
